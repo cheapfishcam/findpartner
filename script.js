02f33357642ac5076282dd086838437f78fcccfa
Mohamed T Safieldeen <40473487+cheapfishcam@users.noreply.github.com>
2018-12-15 19:48:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,15 @@ let patient_names = [
 ]
 
 submitbtn.addEventListener('click' , submitfunc);
-localStorage.clear();
-console.log(localStorage);
+
 
 //pushes user inputs to localStorage
 function submitfunc(){
 let patientname = document.getElementById("patient").value;
 let drname = document.getElementById('dr').value;
 localStorage.setItem(drname, patientname);
-var aywa = localStorage.getItem(drname);
 
 //should clear input value in both fields after user pressses submit (optional)
 
-  console.log(aywa);
-  console.log(localStorage);
+  alert(localStorage);
 }
