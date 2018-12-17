90db6d1b9f199e530520dc6eee2399f8d0db5930
Mohamed T Safieldeen <40473487+cheapfishcam@users.noreply.github.com>
2018-12-18 00:21:54 +0200

    new things

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyBUGUjS7M_QDrwdtY6Af7O8-hc6fA0wink",
+   authDomain: "findpartner-9c012.firebaseapp.com",
+   databaseURL: "https://findpartner-9c012.firebaseio.com",
+   projectId: "findpartner-9c012",
+   storageBucket: "findpartner-9c012.appspot.com",
+   messagingSenderId: "52089296201"
+ };
+ firebase.initializeApp(config);
+
 let submitbtn = document.getElementById('submit');
+let searchbtn = document.getElementById('searchbtn');
 
 
-let drnames = [
-   "Mohamed Tarek-15122",
-   'vdvvv'
-];
+var database = firebase.database();
+var ref = database.ref('list');
 
-let patient_names = [
-  'ffbfbf',
-  'vdvdvd'
-]
+
 
 submitbtn.addEventListener('click' , submitfunc);
 
 
-//pushes user inputs to localStorage
+//pushes user inputs to database
 function submitfunc(){
 let patientname = document.getElementById("patient").value;
 let drname = document.getElementById('dr').value;
-localStorage.setItem(drname, patientname);
+let patientno = document.getElementById('patientno').value;
+let info = {
+   ptname : patientname,
+   fileno : patientno,
+   dr : drname
 
+}
+
+
+ref.push(info);
 //should clear input value in both fields after user pressses submit (optional)
+}
 
-  console.log(localStorage);
+searchbtn.addEventListener('click',findPatient);
+
+function findPatient(){
+  let fileNoinput = document.getElementById('searchpatientname').value;
+
 }
